58d6666878855b2fda78baad52c11aea76e26f1c
Biswajit Basak <juelbasak@gmail.com>
2022-03-27 23:08:57 +0530

    Project Created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { type } from '@testing-library/user-event/dist/type';
+import React from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+    const notify = () => toast("Wow so easy!", { type: "success" });
+
+    return (
+        <div>
+            <button onClick={notify}>Notify!</button>
+            <ToastContainer />
+        </div>
+    );
+}
+
+export default App;
