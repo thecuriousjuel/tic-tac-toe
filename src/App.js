7ca878669484c5dacc19dca08e75fe29db67f168
Biswajit Basak <juelbasak@gmail.com>
2022-03-28 10:51:15 +0530

    Variables declared in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import { type } from '@testing-library/user-event/dist/type';
-import React from 'react';
+import React, { useState } from "react";
+import "./App.css"
+import Icon from "./components/Icon"
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactTostify.css"
 
-function App() {
-    const notify = () => toast("Wow so easy!", { type: "success" });
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap"
+import "bootstrap/dist/css/bootstrap.css"
+import { useState } from "react";
+
+const itemArray = new Array(9).fill("empty")
+
+const App = () => {
+    const [isCross, setIsCross] = useState(false)
+    const [winMsg, setWinMsg] = useState("")
+
+    const reloadGame = () => {
+
+    }
+
+    const checkIsWinner = () => {
+
+    }
+
+    const changeItem = itemNumber => {
+
+    }
+
+
 
     return (
-        <div>
-            <button onClick={notify}>Notify!</button>
-            <ToastContainer />
-        </div>
+
     );
 }
 
-export default App;
+export default AppendMode;
